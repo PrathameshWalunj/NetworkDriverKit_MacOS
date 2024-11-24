d7836f2a7857f2773ec2a66ee5d9a4ccde560300
PrathameshWalunj <prathameshwalunj27@gmail.com>
2024-11-24 03:35:09 -0500

    MyNetworkDriver_IVars instance variables for private data management

--- MyNetworkDriver/MyNetworkDriver/MyNetworkDriver.cpp
+++ MyNetworkDriver/MyNetworkDriver/MyNetworkDriver.cpp
@@ -6,17 +6,35 @@
 //
 
 #include <os/log.h>
-
 #include <DriverKit/IOUserServer.h>
 #include <DriverKit/IOLib.h>
-
+#include <NetworkingDriverKit/IOUserNetworkEthernet.h>
 #include "MyNetworkDriver.h"
 
-kern_return_t
-IMPL(MyNetworkDriver, Start)
-{
-    kern_return_t ret;
-    ret = Start(provider, SUPERDISPATCH);
-    os_log(OS_LOG_DEFAULT, "Hello World");
-    return ret;
-}
+/*
+ *Instance Variable Structure
+ *Structure holds driver's private data
+ *Using separate structure to keep main class clean and organized
+ */
+
+struct MyNetworkDriver_IVars {
+    // Main queue for dispatching driver operations
+    // ### to ensure thread safety in driver
+    IODispatchQueue*                dispatchQueue;
+    
+    // Pool for network packet allocation
+    // This is where we get memory for network packets
+    IOUserNetworkPacketBufferPool*  packetPool;
+    
+    // Queue for packets waiting to be transmitted
+    // Outgoing packets go here before being sent
+    IOUserNetworkPacketQueue*       txQueue;
+    
+    // Queue for received packets
+    // Incoming packets are stored here to be processed
+    IOUserNetworkPacketQueue*       rxQueue;
+    
+    // Current state of interface
+    // true = enabled, false = disbaled
+    bool                            enabled;
+};
